7068bce885c10544223d7ad0f00fcbec1229d3dd
JeffreyMerrickBauman <63916682+JeffreyMerrickBauman@users.noreply.github.com>
2020-05-09 23:47:20 -0500

    PyPoll

--- main.py
+++ main.py
@@ -1,52 +1,39 @@
-#PyBank, main.py
-#print to terminal and with a *.txt file
-#Total Months, Total, Average Change, Greatest Increase in Profits, Greatest Decrease in Profits
+#PyPoll, main.py
+
 
 #importing os and csv modules
 import os
 import csv
 
-#Path test
-#dir_path = os.path.dirname(os.path.realpath(__file__))
-#print(dir_path)   
-
-#import "Resources/budget_data.csv", same structure for GitHub
-#make sure current directory correct for test filepath and eventual GitHub location
-
-csvpath = os.path.join('Resources', 'budget_data.csv')
-#/c/Users/jeffr/Desktop/PyBank/Resources
-
-#test csvpath accuracy
-#print(csvpath)
-#verified correct
+#update for Poll
+csvpath = os.path.join('Resources', 'election_datas.csv')
 
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
 
     #print(csvreader)
-    
-    total = 0 
+
+    Khan = 0
+    Correy = 0
+    Li = 0
+    OTooley = 0
 
     for row in csvreader:
-        total += int(row[1,1:86])
-        #print(row), displays all entries for all columns
+        #print(row)
+        if row[2] == "Khan":
+            Khan += Khan
+        elif row[2] == "Correy":
+            Correy += Correy
+        elif row[2] == "Li":
+            Li += Li
+        elif row[2] == "O'Tooley":
+            OTooley += OTooley
+        
+print(f"Khan: {Khan}")
+print(f"Correy: {Correy}")
+print(f"Li: {Li}")
+print(f"O'Tooley: {OTooley}")
 
-print(total)
-
-# print("Financial Analysis")
-# print("------")
-# print(f"Total Months: {Total_Months}")
-# print(f"Total: {Sum}")
-# print(f"Average Change: {Average_Change}")
-# print(f"Greatest Increase in Profits {Greatest_Profit}")
-# print(f"Greatest Decrease in Profits {Greatest_Loss}")
-
-#Use Sum for Total, Len for Total Months, Avg is Avg or Sum(list)/Len(list)
-
-
-
-
-
-
-
+#Find Winner
+#If statement series or way to use Max?
 
